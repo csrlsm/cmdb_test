5a1eb5e5f02bdb33d329f9758b40b444cec58a74
csrlsm <csrlsm@outlook.com>
2018-02-12 16:05:53 +0800

    20180212

--- ttsx/settings.py
+++ ttsx/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = 'static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR,'static')
-# ]
+#STATIC_ROOT = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
 STATIC_URL = '/static/'
