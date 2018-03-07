ead0bb8d7fadf30f65b0034a03ef1fc599158b21
csrlsm <csrlsm@outlook.com>
2018-03-07 16:12:28 +0800

    a

--- ttsx_app/urls.py
+++ ttsx_app/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
 import views
 urlpatterns = [
     # url(r'^$', views.index),
-    url(r'head', views.head)
+    # url(r'head', views.head),
+    url(r'^login/', views.logins, name='user_login'),
 ]
--- ttsx_app/views.py
+++ ttsx_app/views.py
@@ -4,12 +4,16 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import *
 from django.template import loader, RequestContext
+from  models import *
 
 # Create your views here.
 def index(request):
-    # tmp=loader.get_template('index.html')
-    # return HttpResponse(tmp.render())
-    return render(request, 'index.html')
+    ServerList1 = ServerList.objects.all()[:3]
+    return render(request, 'index.html', locals())
 
 def base(request):
     return render(request, 'base.html')
+
+def logins(request):
+    if request.method ==
+    return render(request, 'login.html', locals())
